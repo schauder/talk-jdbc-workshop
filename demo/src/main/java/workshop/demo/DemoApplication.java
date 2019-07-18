e72ec007ada9690d8a4a0cdbc453db61a42e7793
Jens Schauder <jschauder@pivotal.io>
2019-07-18 10:14:00 +0200

    JPA comparison

--- demo/src/main/java/workshop/demo/DemoApplication.java
+++ demo/src/main/java/workshop/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
 	@Autowired
 	SpeakerRepository speakers;
@@ -15,22 +15,4 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-
-	@Override
-	public void run(String... args) {
-
-		if (speakers != null) {
-			System.out.println("I'm running and have a speakers repository");
-		} else {
-			System.out.println("drats, something is wrong");
-		}
-
-		Speaker martin = new Speaker();
-		martin.name = "Martin Fowler";
-
-		Speaker saved = speakers.save(martin);
-
-		System.out.println(saved);
-
-	}
 }
--- demo/src/test/java/workshop/demo/SpeakerRepositoryTests.java
+++ demo/src/test/java/workshop/demo/SpeakerRepositoryTests.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,4 +28,41 @@ public class SpeakerRepositoryTests {
 
 		assertThat(saved.id).isNotNull();
 	}
+
+
+	@Test
+	public void copyInstance() {
+
+		Speaker martin = new Speaker();
+		martin.name = "Martin Fowler";
+		Speaker saved = speakers.save(martin);
+		assertThat(saved.id).isNotNull();
+
+
+		saved.id = null;
+
+		Speaker savedAgain = speakers.save(martin);
+
+		assertThat(savedAgain.id).isNotNull();
+
+		assertThat(speakers.findAll()).hasSize(2);
+	}
+
+	@Test
+	public void reloadInstance() {
+
+		Speaker martin = new Speaker();
+		martin.name = "Martin Fowler";
+		Speaker saved = speakers.save(martin);
+		assertThat(saved.id).isNotNull();
+
+		Optional<Speaker> loaded = speakers.findById(saved.id);
+		Optional<Speaker> reloaded = speakers.findById(saved.id);
+
+		assertThat(loaded.get())
+				.isEqualTo(reloaded.get())
+				.isNotSameAs(reloaded.get());
+
+	}
+
 }
