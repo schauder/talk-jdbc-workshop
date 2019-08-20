d986382ef165cf1e108e86a5e375ea4fd26eff95
Jens Schauder <jschauder@pivotal.io>
2019-08-20 11:10:07 +0200

    added styles back although they don't seem to have an effect
    rendering to docs folder

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
-asciidoctor-pdf --trace -r asciidoctor-diagram -d book src/main/asciidoc/handout.ad
+asciidoctor-pdf --trace -r asciidoctor-diagram -d book -a pdf-style=src/main/resources/spring-like.yml -a pdf-fontsdir=src/main/resources src/main/asciidoc/handout.ad -o docs/handout.pdf
 
-# -a pdf-style=src/main/resources/spring-like.yml -a pdf-fontsdir=src/main/resources
+
