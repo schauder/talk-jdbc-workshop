332af8eef969d2569e74d121b5a83f1dd13f6231
Jens Schauder <jschauder@pivotal.io>
2019-09-02 16:35:34 +0200

    small stuff, creating fresh pdf

--- demo/src/main/java/workshop/demo/SpeakerRepository.java
+++ demo/src/main/java/workshop/demo/SpeakerRepository.java
@@ -15,7 +15,6 @@
  */
 package workshop.demo;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 interface SpeakerRepository extends CrudRepository<Speaker, Long> {
